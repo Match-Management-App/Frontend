c75c3c7ec28095766ea5e0d7eebe2cedf813889d
zinnnn37 <zinnnn37@gmail.com>
2024-07-15 21:18:32 +0900

    style: Color Theme class 정의

---
+++ wanna_play_soccer/lib/Theme/my_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color primaryMint = Color(0xff1EC1A4);
+
+  static const Color myBlack = Color(0xff20282D);
+  static const Color gradientGrey = Color(0xff38464F);
+
+  static const Color myDarkGrey = Color(0xff526068);
+  static const Color myGrey = Color(0xffA5B1B7);
+  static const Color myLightGrey = Color(0xffD6DDE2);
+
+  static const Color myWhite = Color(0xffF5F9FC);
+  static const Color myPointWhite = Color(0xffF4F4E1);
+}
