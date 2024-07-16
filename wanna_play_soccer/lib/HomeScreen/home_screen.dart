ea3b412a7d6e6f28af8201529462cf30f2410998
zinnnn37 <zinnnn37@gmail.com>
2024-07-16 14:32:33 +0900

    refactor: AppBar class 파일로 분리

--- wanna_play_soccer/lib/HomeScreen/home_screen.dart
+++ wanna_play_soccer/lib/HomeScreen/home_screen.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:wanna_play_soccer/HomeScreen/main_icon.dart';
+import 'package:wanna_play_soccer/HomeScreen/my_app_bar.dart';
 import 'package:wanna_play_soccer/Theme/my_colors.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -59,38 +58,4 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-}
-
-class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MyAppBar({super.key});
-
-  // v1/users/name
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      elevation: 0,
-      scrolledUnderElevation: 0,
-      backgroundColor: MyColors.myBlack,
-      title: Container(
-        padding: const EdgeInsets.all(20.0),
-        child: const Text(
-          '반갑습니다 User님 👏',
-          style: TextStyle(color: MyColors.myWhite),
-        ),
-      ),
-      centerTitle: false,
-      flexibleSpace: ClipRRect(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-          child: Container(
-            color: Colors.transparent,
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
---
+++ wanna_play_soccer/lib/HomeScreen/my_app_bar.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:wanna_play_soccer/Theme/my_colors.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({super.key});
+
+  // v1/users/name
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      backgroundColor: MyColors.myBlack,
+      title: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: const Text(
+          '반갑습니다 User님 👏',
+          style: TextStyle(color: MyColors.myWhite),
+        ),
+      ),
+      centerTitle: false,
+      flexibleSpace: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            color: Colors.transparent,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
