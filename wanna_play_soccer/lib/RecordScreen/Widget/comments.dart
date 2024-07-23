c50553a5622bd13965ad7ad061e693e478b22012
zinnnn37 <zinnnn37@gmail.com>
2024-07-23 21:14:14 +0900

    feat: 댓글 목록 대충 만들어봄

--- wanna_play_soccer/lib/RecordScreen/Widget/comments.dart
+++ wanna_play_soccer/lib/RecordScreen/Widget/comments.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:wanna_play_soccer/Theme/my_colors.dart';
 
 class CommentSection extends StatefulWidget {
@@ -20,6 +19,25 @@ class _CommentSectionState extends State<CommentSection> {
     super.dispose();
   }
 
+  final List<Comment> commentHistory = [
+    Comment(
+        userName: "김철수", comment: "오늘 경기 정말 재미있었어요!", date: "2024-07-20 20:20"),
+    Comment(
+        userName: "이영희",
+        comment: "다음 경기도 기대됩니다. 화이팅!",
+        date: "2024-07-20 21:15"),
+    Comment(
+        userName: "박민준", comment: "선수들 모두 수고하셨습니다.", date: "2024-07-21 09:30"),
+    Comment(
+        userName: "정수연",
+        comment: "경기장 분위기가 너무 좋았어요.",
+        date: "2024-07-21 14:45"),
+    Comment(
+        userName: "강지훈",
+        comment: "다음에는 꼭 직접 가서 응원하고 싶네요!",
+        date: "2024-07-22 10:05"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,6 +50,21 @@ class _CommentSectionState extends State<CommentSection> {
           children: [
             // 댓글 입력
             CommentInput(controller: _controller),
+            // * Text(widget.date.toString().split(' ')[0]),
+            // 댓글 목록
+            Container(
+              padding: const EdgeInsets.only(top: 20),
+              // Column으로 commentHistory를 뿌려줌
+              child: Column(
+                children: commentHistory
+                    .map((comment) => CommentTile(
+                          name: comment.userName,
+                          comment: comment.comment,
+                          date: comment.date,
+                        ))
+                    .toList(),
+              ),
+            ),
           ],
         ),
       ),
@@ -118,4 +151,70 @@ class CommentInput extends StatelessWidget {
       ),
     );
   }
+}
+
+class CommentTile extends StatelessWidget {
+  const CommentTile({
+    super.key,
+    required this.name,
+    required this.comment,
+    required this.date,
+  });
+
+  final String name;
+  final String comment;
+  final String date;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                name,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: MyColors.myWhite,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                date,
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: MyColors.myGrey,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text(
+            comment,
+            style: const TextStyle(
+              fontSize: 16,
+              color: MyColors.myWhite,
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
+
+class Comment {
+  final String userName;
+  final String comment;
+  final String date;
+
+  Comment({
+    required this.userName,
+    required this.comment,
+    required this.date,
+  });
 }
