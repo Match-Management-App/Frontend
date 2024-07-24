2836f748fe5a58f3b45f89b2f1e9839cd4c404d3
zinnnn37 <zinnnn37@gmail.com>
2024-07-24 15:57:46 +0900

    refactor: 명예의 전당 class 분리

---
+++ wanna_play_soccer/lib/RecordScreen/Widget/hall_of_fame.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:wanna_play_soccer/FindPartner/Widget/partner_rank.dart';
+import 'package:wanna_play_soccer/Theme/my_colors.dart';
+import 'package:wanna_play_soccer/Theme/my_theme.dart';
+import 'package:wanna_play_soccer/unused/attendance.dart';
+
+class HallOfFame extends StatefulWidget {
+  const HallOfFame({super.key});
+
+  @override
+  State<HallOfFame> createState() => _HallOfFameState();
+}
+
+class _HallOfFameState extends State<HallOfFame> {
+  late final PageController _controller;
+
+  final List<Widget> _pages = [
+    const HOF(title: "득점"),
+    const HOF(title: "어시스트"),
+    const HOF(title: "수비"),
+    const HOF(title: "출석"),
+  ];
+
+  Widget itemBuilder(BuildContext context, int index) {
+    // for infinite loop
+    return _pages[index % _pages.length];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // for infinite loop
+    _controller = PageController(initialPage: 500);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 15),
+      width: double.infinity,
+      // height: 200,
+      padding: const EdgeInsets.all(20),
+      decoration: MyTheme.widgetDecoration,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 200,
+            child: PageView.builder(
+              controller: _controller,
+              itemBuilder: itemBuilder,
+            ),
+          ),
+          SmoothPageIndicator(
+            controller: _controller,
+            count: _pages.length,
+            effect: const SlideEffect(
+              dotWidth: 8,
+              dotHeight: 8,
+              activeDotColor: MyColors.primaryMint,
+              dotColor: MyColors.widgetGrey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HOF extends StatelessWidget {
+  const HOF({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Header3(text: title),
+        ),
+        const Ranking(first: "first", second: "second", third: "third"),
+      ],
+    );
+  }
+}
